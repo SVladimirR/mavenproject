a68cffa90ec076a2682cc9ae3435b3e2ba68bd04
SVladimirR <selitrennikov.vr@gmail.com>
2023-06-19 01:03:46 +0300

    Homework 17

--- hw17/src/test/java/hw17/MethodArrayCheckCompositionTest.java
+++ hw17/src/test/java/hw17/MethodArrayCheckCompositionTest.java
@@ -7,21 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class MethodArrayCheckCompositionTest {
 
     @Test
-    void checkArray() {
+    void checkArray_1() {
         assertEquals(false, new MethodArrayCheckComposition().checkArray(new int[]{1, 1}));
     }
 
     @Test
-    void checkArray_1() {
+    void checkArray_2() {
         assertEquals(false, new MethodArrayCheckComposition().checkArray(new int[]{4, 4, 4}));
     }
 
     @Test
-    void checkArray_2() {
+    void checkArray_3() {
         assertEquals(true, new MethodArrayCheckComposition().checkArray(new int[]{1, 1, 4, 4}));
     }
     @Test
-    void checkArray_3() {
+    void checkArray_4() {
         assertEquals(false, new MethodArrayCheckComposition().checkArray(new int[]{3, 5, 2, 6}));
     }
 }
--- hw17/src/test/java/hw17/MethodArrayCheckTest.java
+++ hw17/src/test/java/hw17/MethodArrayCheckTest.java
@@ -8,22 +8,22 @@ class MethodArrayCheckTest {
 
 
     @Test
-    void arrayCheckingElements() {
-        assertArrayEquals(new int[]{1, 7}, new MethodArrayCheck().arrayCheckingElements(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7}, 4));
-        assertArrayEquals(new int[]{2, 3, 1, 7}, new MethodArrayCheck().arrayCheckingElements(new int[]{1, 2, 4, 4, 2, 3, 1, 7}, 4));
-
-    }
-
-    @Test
     void arrayCheckingElements_1() {
-
-        assertArrayEquals(new int[]{2, 3, 1, 7}, new MethodArrayCheck().arrayCheckingElements(new int[]{1, 2, 4, 4, 2, 3, 1, 7}, 4));
-
+        assertArrayEquals(new int[]{1, 7}, new MethodArrayCheck().arrayCheckingElements(new int[]{1, 2, 4, 4, 2, 3, 4, 1, 7}, 4));
     }
 
     @Test
     void arrayCheckingElements_2() {
+        assertArrayEquals(new int[]{2, 3, 1, 7}, new MethodArrayCheck().arrayCheckingElements(new int[]{1, 2, 4, 4, 2, 3, 1, 7}, 4));
+    }
 
+    @Test
+    void arrayCheckingElements_3() {
+        assertArrayEquals(new int[]{2, 2, 3, 1, 7}, new MethodArrayCheck().arrayCheckingElements(new int[]{1, 4, 2, 2, 3, 1, 7}, 4));
+    }
+
+    @Test
+    void arrayCheckingElements_4() {
         assertThrows(RuntimeException.class, () -> new MethodArrayCheck().arrayCheckingElements(new int[]{1, 2, 2, 3, 1, 7}, 4));
     }
 
